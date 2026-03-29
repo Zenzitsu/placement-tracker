61c19311b9d9003d1e59a4aca269bebe88aade4c
Parth Sahu <parthsahu100@gmail.com>
2026-03-29 23:39:29 +0530

    Fix some issues

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ initializeDatabase();
 
 // Create Express app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.BACKEND_PORT || 5000;
 
 // Middleware
 app.use(cors());
